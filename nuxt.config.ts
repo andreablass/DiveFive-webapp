ae6b91e1aea37a8da3251a7a5a6355784b9a9d70
andreablass <andreablass11@gmail.com>
2025-09-02 13:00:46 -0600

    WIP
      - reset: nuevo proyecto con Vue + Vite para marketing

--- nuxt.config.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtConfig({
-  css: ['~/assets/tailwind.css'],
-  runtimeConfig: {
-    public: {
-      supabaseUrl: process.env.NUXT_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.NUXT_PUBLIC_SUPABASE_ANON_KEY
-    }
-  }
-})
